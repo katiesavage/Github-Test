c247bcd0a96d67269a29f8783976dfcf8c0b795d
Kathleen Savage <savage.katie@kathleens-macbook-pro.local>
2019-02-12 21:08:22 -0800

    Initial Commit

---
+++ Github Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Github Test
+//
+//  Created by Kathleen Savage on 2/12/19.
+//  Copyright © 2019 Kathleen Savage. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
